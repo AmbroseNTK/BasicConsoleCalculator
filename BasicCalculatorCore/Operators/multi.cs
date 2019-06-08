1a9912028c256bd3d553b2b9f156d073d5e894bd
Huynh Trong Nhan <Huynh Trong Nhan@DESKTOP-K3CMS0I>
2019-06-08 12:19:31 +0700

    add multi and division

---
+++ BasicCalculatorCore/Operators/multi.cs
@@ -0,0 +1,20 @@
+﻿using BasicCalculatorCore.Operators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCalculatorCore.operators
+{
+    class multi : IOperable, IBiOperator
+    {
+        public object Calc(object input1, object input2)
+        {
+            return (int)input1 * (int)input2;
+        }
+
+        public string Info()
+        {
+            return "Calc a * b";
+        }
+    }
+}
