18d254e63dea2dbdd95ad9aa8f5b13c2bdb36d1e
Hoang Duy <duy.th1305@sinhvien.hoasen.edu.vn>
2019-06-08 12:31:28 +0700

    Cal Total Area of the Circle

---
+++ BasicCalculatorCore/Operators/CircleArea.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCalculatorCore.Operators
+{
+    class CircleArea : IOperable, IBiOperator
+    {
+        /// <summary>
+        /// Tinh dien tich duong tron
+        /// </summary>
+        public object Calc(object input1)
+        {
+            if ((float)input1<0)
+            {
+                return null;
+            }
+            else
+            { return (float)input1 * 3.14;
+            }
+            
+        }
+
+        public string Info()
+        {
+            return "Calc CircleArea pi * (r*r)";
+        }
+    }
+}
