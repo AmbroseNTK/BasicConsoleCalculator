77da67930335f753518d07ec0196ed309595a993
Dang Cong Hao <hao.dc2158@sinhvien.hoasen.edu.vn>
2019-06-08 12:01:44 +0700

    Create Sqrt function and Sin function

---
+++ BasicCalculatorCore/Operators/Sin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCalculatorCore.Operators
+{
+    class Sin : IOperable, IMonoOperator
+    {
+        public object Calc(object input)
+        {
+            return Math.Sin((double)input);
+        }
+
+        public string Info()
+        {
+            return "sin of x";
+        }
+    }
+}
