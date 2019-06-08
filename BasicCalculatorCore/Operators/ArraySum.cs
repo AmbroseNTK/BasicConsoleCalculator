6cce23b1c49d5bd4313a805132831a21097c86e8
LAC.DG5070 2175070 <LAC.DG5070@SINHVIEN.HOASEN.EDU.VN>
2019-06-08 12:28:21 +0700

    Sum and Time Array elements

---
+++ BasicCalculatorCore/Operators/ArraySum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BasicCalculatorCore.Operators;
+
+public class ArraySum : IMonoOperator, IOperable
+{
+    public object[] input;
+    public object Length { get; }
+    public object Sum;
+    public object Calc(object input)
+    {
+        Sum = input[0];
+        for (object i = 1; i < input.Length; i++)
+        {
+            Sum += input[i];
+        }
+        return input;
+    }
+}
---
+++ BasicCalculatorCore/Operators/ArrayTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BasicCalculatorCore.Operators;
+
+namespace BasicCalculatorCore.Operators
+{
+    public class ArrayTime : IMonoOperator, IOperable
+    {
+        public object[] input;
+        public object Length { get; }
+        public object Time;
+        public object Calc(object input)
+        {
+            Time = input[0];
+            for (object i = 1; i < input.Length; i++)
+            {
+                Sum *= input[i];
+            }
+            return input;
+        }
+    }
+}
